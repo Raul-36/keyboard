58a3048771da0b945c022d14853d338a9c88bad0
Rau-36 <raul363636r36d@gmail.com>
2023-06-20 17:17:33 +0400

    adding keyboard output and logic

--- keyboard/keyboard/Form1.Designer.cs
+++ keyboard/keyboard/Form1.Designer.cs
@@ -28,12 +28,422 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            QButton = new Button();
+            WButton = new Button();
+            EButton = new Button();
+            RButton = new Button();
+            TButton = new Button();
+            YButton = new Button();
+            UButton = new Button();
+            IButton = new Button();
+            OButton = new Button();
+            PButton = new Button();
+            AButton = new Button();
+            SButton = new Button();
+            DButton = new Button();
+            FButton = new Button();
+            GButton = new Button();
+            HButton = new Button();
+            JButton = new Button();
+            KButton = new Button();
+            LButton = new Button();
+            ZButton = new Button();
+            XButton = new Button();
+            CButton = new Button();
+            VButton = new Button();
+            BButton = new Button();
+            NButton = new Button();
+            MButton = new Button();
+            SpaceButton = new Button();
+            BackspaceButton = new Button();
+            RichTextBox = new RichTextBox();
+            SuspendLayout();
+            // 
+            // QButton
+            // 
+            QButton.BackColor = SystemColors.ControlLight;
+            QButton.Location = new Point(35, 161);
+            QButton.Name = "QButton";
+            QButton.Size = new Size(58, 51);
+            QButton.TabIndex = 0;
+            QButton.Text = "q";
+            QButton.UseVisualStyleBackColor = false;
+            QButton.Click += AddToTextbox;
+            // 
+            // WButton
+            // 
+            WButton.BackColor = SystemColors.ControlLight;
+            WButton.Location = new Point(99, 161);
+            WButton.Name = "WButton";
+            WButton.Size = new Size(58, 51);
+            WButton.TabIndex = 1;
+            WButton.Text = "w";
+            WButton.UseVisualStyleBackColor = false;
+            WButton.Click += AddToTextbox;
+            // 
+            // EButton
+            // 
+            EButton.BackColor = SystemColors.ControlLight;
+            EButton.Location = new Point(163, 161);
+            EButton.Name = "EButton";
+            EButton.Size = new Size(58, 51);
+            EButton.TabIndex = 2;
+            EButton.Text = "e";
+            EButton.UseVisualStyleBackColor = false;
+            EButton.Click += AddToTextbox;
+            // 
+            // RButton
+            // 
+            RButton.BackColor = SystemColors.ControlLight;
+            RButton.Location = new Point(227, 161);
+            RButton.Name = "RButton";
+            RButton.Size = new Size(58, 51);
+            RButton.TabIndex = 3;
+            RButton.Text = "r";
+            RButton.UseVisualStyleBackColor = false;
+            RButton.Click += AddToTextbox;
+            // 
+            // TButton
+            // 
+            TButton.BackColor = SystemColors.ControlLight;
+            TButton.Location = new Point(291, 161);
+            TButton.Name = "TButton";
+            TButton.Size = new Size(58, 51);
+            TButton.TabIndex = 4;
+            TButton.Text = "t";
+            TButton.UseVisualStyleBackColor = false;
+            TButton.Click += AddToTextbox;
+            // 
+            // YButton
+            // 
+            YButton.BackColor = SystemColors.ControlLight;
+            YButton.Location = new Point(355, 161);
+            YButton.Name = "YButton";
+            YButton.Size = new Size(58, 51);
+            YButton.TabIndex = 5;
+            YButton.Text = "y";
+            YButton.UseVisualStyleBackColor = false;
+            YButton.Click += AddToTextbox;
+            // 
+            // UButton
+            // 
+            UButton.BackColor = SystemColors.ControlLight;
+            UButton.Location = new Point(419, 161);
+            UButton.Name = "UButton";
+            UButton.Size = new Size(58, 51);
+            UButton.TabIndex = 6;
+            UButton.Text = "u";
+            UButton.UseVisualStyleBackColor = false;
+            UButton.Click += AddToTextbox;
+            // 
+            // IButton
+            // 
+            IButton.BackColor = SystemColors.ControlLight;
+            IButton.Location = new Point(483, 161);
+            IButton.Name = "IButton";
+            IButton.Size = new Size(58, 51);
+            IButton.TabIndex = 7;
+            IButton.Text = "i";
+            IButton.UseVisualStyleBackColor = false;
+            IButton.Click += AddToTextbox;
+            // 
+            // OButton
+            // 
+            OButton.BackColor = SystemColors.ControlLight;
+            OButton.Location = new Point(547, 161);
+            OButton.Name = "OButton";
+            OButton.Size = new Size(58, 51);
+            OButton.TabIndex = 8;
+            OButton.Text = "o";
+            OButton.UseVisualStyleBackColor = false;
+            OButton.Click += AddToTextbox;
+            // 
+            // PButton
+            // 
+            PButton.BackColor = SystemColors.ControlLight;
+            PButton.Location = new Point(611, 161);
+            PButton.Name = "PButton";
+            PButton.Size = new Size(58, 51);
+            PButton.TabIndex = 9;
+            PButton.Text = "p";
+            PButton.UseVisualStyleBackColor = false;
+            PButton.Click += AddToTextbox;
+            // 
+            // AButton
+            // 
+            AButton.BackColor = SystemColors.ControlLight;
+            AButton.Location = new Point(60, 218);
+            AButton.Name = "AButton";
+            AButton.Size = new Size(58, 51);
+            AButton.TabIndex = 10;
+            AButton.Text = "a";
+            AButton.UseVisualStyleBackColor = false;
+            AButton.Click += AddToTextbox;
+            // 
+            // SButton
+            // 
+            SButton.BackColor = SystemColors.ControlLight;
+            SButton.Location = new Point(124, 218);
+            SButton.Name = "SButton";
+            SButton.Size = new Size(58, 51);
+            SButton.TabIndex = 11;
+            SButton.Text = "s";
+            SButton.UseVisualStyleBackColor = false;
+            SButton.Click += AddToTextbox;
+            // 
+            // DButton
+            // 
+            DButton.BackColor = SystemColors.ControlLight;
+            DButton.Location = new Point(188, 218);
+            DButton.Name = "DButton";
+            DButton.Size = new Size(58, 51);
+            DButton.TabIndex = 12;
+            DButton.Text = "d";
+            DButton.UseVisualStyleBackColor = false;
+            DButton.Click += AddToTextbox;
+            // 
+            // FButton
+            // 
+            FButton.BackColor = SystemColors.ControlLight;
+            FButton.Location = new Point(252, 218);
+            FButton.Name = "FButton";
+            FButton.Size = new Size(58, 51);
+            FButton.TabIndex = 13;
+            FButton.Text = "f";
+            FButton.UseVisualStyleBackColor = false;
+            FButton.Click += AddToTextbox;
+            // 
+            // GButton
+            // 
+            GButton.BackColor = SystemColors.ControlLight;
+            GButton.Location = new Point(316, 218);
+            GButton.Name = "GButton";
+            GButton.Size = new Size(58, 51);
+            GButton.TabIndex = 14;
+            GButton.Text = "g";
+            GButton.UseVisualStyleBackColor = false;
+            GButton.Click += AddToTextbox;
+            // 
+            // HButton
+            // 
+            HButton.BackColor = SystemColors.ControlLight;
+            HButton.Location = new Point(380, 218);
+            HButton.Name = "HButton";
+            HButton.Size = new Size(58, 51);
+            HButton.TabIndex = 15;
+            HButton.Text = "h";
+            HButton.UseVisualStyleBackColor = false;
+            HButton.Click += AddToTextbox;
+            // 
+            // JButton
+            // 
+            JButton.BackColor = SystemColors.ControlLight;
+            JButton.Location = new Point(444, 218);
+            JButton.Name = "JButton";
+            JButton.Size = new Size(58, 51);
+            JButton.TabIndex = 16;
+            JButton.Text = "j";
+            JButton.UseVisualStyleBackColor = false;
+            JButton.Click += AddToTextbox;
+            // 
+            // KButton
+            // 
+            KButton.BackColor = SystemColors.ControlLight;
+            KButton.Location = new Point(508, 218);
+            KButton.Name = "KButton";
+            KButton.Size = new Size(58, 51);
+            KButton.TabIndex = 17;
+            KButton.Text = "k";
+            KButton.UseVisualStyleBackColor = false;
+            KButton.Click += AddToTextbox;
+            // 
+            // LButton
+            // 
+            LButton.BackColor = SystemColors.ControlLight;
+            LButton.Location = new Point(572, 218);
+            LButton.Name = "LButton";
+            LButton.Size = new Size(58, 51);
+            LButton.TabIndex = 18;
+            LButton.Text = "l";
+            LButton.UseVisualStyleBackColor = false;
+            LButton.Click += AddToTextbox;
+            // 
+            // ZButton
+            // 
+            ZButton.BackColor = SystemColors.ControlLight;
+            ZButton.Location = new Point(99, 275);
+            ZButton.Name = "ZButton";
+            ZButton.Size = new Size(58, 51);
+            ZButton.TabIndex = 19;
+            ZButton.Text = "z";
+            ZButton.UseVisualStyleBackColor = false;
+            ZButton.Click += AddToTextbox;
+            // 
+            // XButton
+            // 
+            XButton.BackColor = SystemColors.ControlLight;
+            XButton.Location = new Point(163, 275);
+            XButton.Name = "XButton";
+            XButton.Size = new Size(58, 51);
+            XButton.TabIndex = 20;
+            XButton.Text = "x";
+            XButton.UseVisualStyleBackColor = false;
+            XButton.Click += AddToTextbox;
+            // 
+            // CButton
+            // 
+            CButton.BackColor = SystemColors.ControlLight;
+            CButton.Location = new Point(227, 275);
+            CButton.Name = "CButton";
+            CButton.Size = new Size(58, 51);
+            CButton.TabIndex = 21;
+            CButton.Text = "c";
+            CButton.UseVisualStyleBackColor = false;
+            CButton.Click += AddToTextbox;
+            // 
+            // VButton
+            // 
+            VButton.BackColor = SystemColors.ControlLight;
+            VButton.Location = new Point(291, 275);
+            VButton.Name = "VButton";
+            VButton.Size = new Size(58, 51);
+            VButton.TabIndex = 22;
+            VButton.Text = "v";
+            VButton.UseVisualStyleBackColor = false;
+            VButton.Click += AddToTextbox;
+            // 
+            // BButton
+            // 
+            BButton.BackColor = SystemColors.ControlLight;
+            BButton.Location = new Point(355, 275);
+            BButton.Name = "BButton";
+            BButton.Size = new Size(58, 51);
+            BButton.TabIndex = 23;
+            BButton.Text = "b";
+            BButton.UseVisualStyleBackColor = false;
+            BButton.Click += AddToTextbox;
+            // 
+            // NButton
+            // 
+            NButton.BackColor = SystemColors.ControlLight;
+            NButton.Location = new Point(419, 275);
+            NButton.Name = "NButton";
+            NButton.Size = new Size(58, 51);
+            NButton.TabIndex = 24;
+            NButton.Text = "n";
+            NButton.UseVisualStyleBackColor = false;
+            NButton.Click += AddToTextbox;
+            // 
+            // MButton
+            // 
+            MButton.BackColor = SystemColors.ControlLight;
+            MButton.Location = new Point(483, 275);
+            MButton.Name = "MButton";
+            MButton.Size = new Size(58, 51);
+            MButton.TabIndex = 25;
+            MButton.Text = "m";
+            MButton.UseVisualStyleBackColor = false;
+            MButton.Click += AddToTextbox;
+            // 
+            // SpaceButton
+            // 
+            SpaceButton.BackColor = SystemColors.ControlLight;
+            SpaceButton.Location = new Point(227, 332);
+            SpaceButton.Name = "SpaceButton";
+            SpaceButton.Size = new Size(314, 51);
+            SpaceButton.TabIndex = 26;
+            SpaceButton.Text = "Space";
+            SpaceButton.UseVisualStyleBackColor = false;
+            SpaceButton.Click += AddToTextbox;
+            // 
+            // BackspaceButton
+            // 
+            BackspaceButton.BackColor = SystemColors.ControlLight;
+            BackspaceButton.Location = new Point(659, 104);
+            BackspaceButton.Name = "BackspaceButton";
+            BackspaceButton.Size = new Size(140, 51);
+            BackspaceButton.TabIndex = 27;
+            BackspaceButton.Text = "Backspace";
+            BackspaceButton.UseVisualStyleBackColor = false;
+            BackspaceButton.Click += DeleteToTextbox;
+            // 
+            // RichTextBox
+            // 
+            RichTextBox.Location = new Point(12, 12);
+            RichTextBox.Name = "RichTextBox";
+            RichTextBox.Size = new Size(641, 107);
+            RichTextBox.TabIndex = 28;
+            RichTextBox.Text = "";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(RichTextBox);
+            Controls.Add(BackspaceButton);
+            Controls.Add(SpaceButton);
+            Controls.Add(MButton);
+            Controls.Add(NButton);
+            Controls.Add(BButton);
+            Controls.Add(VButton);
+            Controls.Add(CButton);
+            Controls.Add(XButton);
+            Controls.Add(ZButton);
+            Controls.Add(LButton);
+            Controls.Add(KButton);
+            Controls.Add(JButton);
+            Controls.Add(HButton);
+            Controls.Add(GButton);
+            Controls.Add(FButton);
+            Controls.Add(DButton);
+            Controls.Add(SButton);
+            Controls.Add(AButton);
+            Controls.Add(PButton);
+            Controls.Add(OButton);
+            Controls.Add(IButton);
+            Controls.Add(UButton);
+            Controls.Add(YButton);
+            Controls.Add(TButton);
+            Controls.Add(RButton);
+            Controls.Add(EButton);
+            Controls.Add(WButton);
+            Controls.Add(QButton);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private Button QButton;
+        private Button WButton;
+        private Button EButton;
+        private Button RButton;
+        private Button TButton;
+        private Button YButton;
+        private Button UButton;
+        private Button IButton;
+        private Button OButton;
+        private Button PButton;
+        private Button AButton;
+        private Button SButton;
+        private Button DButton;
+        private Button FButton;
+        private Button GButton;
+        private Button HButton;
+        private Button JButton;
+        private Button KButton;
+        private Button LButton;
+        private Button ZButton;
+        private Button XButton;
+        private Button CButton;
+        private Button VButton;
+        private Button BButton;
+        private Button NButton;
+        private Button MButton;
+        private Button SpaceButton;
+        private Button BackspaceButton;
+        private RichTextBox RichTextBox;
     }
 }
--- keyboard/keyboard/Form1.cs
+++ keyboard/keyboard/Form1.cs
@@ -6,5 +6,25 @@ namespace keyboard
         {
             InitializeComponent();
         }
+
+        private void AddToTextbox(object sender, EventArgs e)
+        {
+            if (sender is Button button)
+            {
+                if (button.Text == "Space")
+                    this.RichTextBox.Text += " ";
+                else
+                    this.RichTextBox.Text += button.Text;
+            }
+        }
+        private void DeleteToTextbox(object sender, EventArgs e)
+        {
+            if (this.RichTextBox.Text.Length == 0)
+            {
+                return;
+            }
+            string text = this.RichTextBox.Text;
+            this.RichTextBox.Text = text.Substring(0, text.Length - 1);
+        }
     }
 }
